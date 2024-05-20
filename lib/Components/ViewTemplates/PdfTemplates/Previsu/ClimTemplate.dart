004cf16d5430138dbfbe67a30dd24c4708230930
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-05-20 15:15:34 +0200

    remaniement presque fini (sans pdf button)

--- lib/Components/ViewTemplates/PdfTemplates/Previsu/ClimTemplate.dart
+++ lib/Components/ViewTemplates/PdfTemplates/Previsu/ClimTemplate.dart
@@ -93,6 +93,10 @@ class _ClimTemplateState extends State<ClimTemplates> {
   List<Map<String, String>> headerData = [];
   List<List<Map<String, String>>> infoChantierData = [];
   List<List<Map<String, String>>> infoTechData = [];
+  List<List<Map<String, String>>> observData = [];
+  List<List<Map<String, String>>> uniteData = [];
+  List<List<Map<String, String>>> groupData = [];
+
   @override
   Widget build(BuildContext context) {
 
@@ -129,7 +133,59 @@ class _ClimTemplateState extends State<ClimTemplates> {
         {"title" : "Modèle de l'unité : ", "content" : widget.modele},
       ],
       [
+        {"title" : "Référence : ", "content" : widget.reference},
+      ],
+    ];
+
+    observData = [
+      [
+        {"title" : "État : ", "content" : "${widget.etat}/10"},
+        {"title" : "Passage du testeur de fuite : ", "content" : boolToString(widget.testFuite)},
+      ],
+      [
         {"title" : "Remarques : ", "content" : widget.observD},
+      ],
+    ];
+    
+    uniteData = [
+      [
+        {"title" : "Température d'arrivée : ", "content" : "${widget.cold == ""? "non testée" : widget.cold}° pour le mode froid et ${widget.hot == ""? "non testée" : widget.hot}° pour le mode chaud."},
+      ],
+      [
+        {"title" : "Dépose du carénage : ", "content" : boolToString(widget.carenage)},
+        {"title" : "Dépose des ventilos : ", "content" : boolToString(widget.ventilo)},
+      ],
+      [
+        {"title" : "Dépose du bac à condensation : ", "content" : boolToString(widget.condensa)},
+        {"title" : "Nettoyage, dégraissage, etc : ", "content" : boolToString(widget.clean)},
+      ],
+      [
+        {"title" : "Remontage de l'ensemble : ", "content" : boolToString(widget.assemble)},
+        {"title" : "Nettoyage du filtre: ", "content" : boolToString(widget.cleanFiltre)},
+      ],
+      [
+        {"title" : "Test des condensations : ", "content" : boolToString(widget.testCondensa)},
+      ],
+      [
+        {"title" : "Remarques : ", "content" : widget.observD2},
+      ]
+    ];
+
+    groupData = [
+      [
+        {"title" : "Type de compresseur : ", "content" : widget.typeExt},
+        {"title" : "Dépose du capot : ", "content" : boolToString(widget.capot)},
+      ],
+      [
+        {"title" : "Aspiration et brossage : ", "content" : boolToString(widget.aspiBros)},
+        {"title" : "Nettoyage et rinçage : ", "content" : boolToString(widget.cleanRin)},
+      ],
+      [
+        {"title" : "Vérification des supports : ", "content" : boolToString(widget.verifSupp)},
+        {"title" : "Vérification des ventilos : ", "content" : boolToString(widget.verifVentil)},
+      ],
+      [
+        {"title" : "Vérification du raccord des cuivres : ", "content" : boolToString(widget.verifCuivre)},
       ],
     ];
 
@@ -139,14 +195,22 @@ class _ClimTemplateState extends State<ClimTemplates> {
   ///////////////////////////////////////////////////////////////
   /// corp du widget ClimTemplate
   Widget body() {
+
+    List<String> compTitle = ["Informations de chantier", "Informations techniques", "Observations visuelles", "Unité intérieure", "Groupe extérieur"];    
+    List<List<List<Map<String, String>>>> compData = [infoChantierData, infoTechData, observData, uniteData, groupData];
+
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Gap(10),
           header(),
-          infoChantier(),
-          infoTech()
+          Column(
+            children: List.generate(
+              compTitle.length,
+              (index) => presData(compTitle[index], compData[index])
+            ),
+          )
         ],
       ),
     );
@@ -173,17 +237,17 @@ class _ClimTemplateState extends State<ClimTemplates> {
   }
 
   ///////////////////////////////////////////////////////////////
-  /// partie 2 avec les infos de chantier
-  Widget infoChantier() {
+  /// mes en forme les données avec les bons composants
+  Widget presData(String title, List<List<Map<String, String>>> data) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.95,
       child: Column(
         children: [
           const Gap(10),
           InfoBlock(
-            title: "Informations de chantier",
+            title: title,
             content: InfoDoubleLine(
-              lines: infoChantierData
+              lines: data
             )
           )
         ],
@@ -191,22 +255,4 @@ class _ClimTemplateState extends State<ClimTemplates> {
     );
   }
 
-  ///////////////////////////////////////////////////////////////
-  /// partie 3 avec les infos de chantier
-  Widget infoTech() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.95,
-      child: Column(
-        children: [
-          const Gap(10),
-          InfoBlock(
-            title: "Informations techniques",
-            content: InfoDoubleLine(
-              lines: infoTechData
-            )
-          )
-        ],
-      ),
-    );
-  }
 }
